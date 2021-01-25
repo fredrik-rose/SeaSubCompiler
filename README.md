dd49266f202d7bb6312d63deabcfaacdf3067b16
Fredrik Rose <fredrik.rose.developer@gmail.com>
2021-01-25 20:16:08 +0100

    Create the lexer module of the sea sub compiler
    Currently only able to lex simple (potentially multi-line) math
    expressions including parentheses.

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ A compiler for a small subset (sub) of the C (sea) programming language.
 
 This section describes the architecture of the sea sub compiler.
 
+### Lexer
+
+The lexical analyzer (also known as scanner or tokenizer). This is the first step of the compiler that takes the raw
+sea sub source code as input and outputs tokens (e.g. numbers, brackets, operators, etc.). It performs the first part
+of the syntax check as it only accepts tokens that are part of the sea sub language.
+
 ### Error Handler
 
 This component is responsible for the error handling and is used by all parts of the compiler.
---
+++ seasub/lexer.py
@@ -0,0 +1,73 @@
+"""
+The lexical analyzer (also known as scanner or tokenizer) of the sea sub compiler.
+"""
+import re
+
+from seasub import error_handler as err
+
+
+class Lexer:
+    def __init__(self, text):
+        self._tokenizer = tokenize(text)
+        self._advance()
+
+    def peek(self):
+        return self._current
+
+    def eat(self, token_type):
+        current = self._current
+        if current.type != token_type:
+            raise err.SeaSubSyntaxError(f"Unexpected {current.value!r} on line {current.line}:{current.column}")
+        self._advance()
+        return current
+
+    def _advance(self):
+        try:
+            self._current = next(self._tokenizer)
+        except StopIteration:
+            self._current = None
+
+
+def tokenize(text):
+    token_specification = [
+        ('NUMBER', r'\d+(\.\d*)?'),  # Integer or decimal number.
+        ('ARITHMETIC_OPERATOR', r'[+\-*/]'),  # Arithmetic operators.
+        ('LEFT_PARENTHESIS', r'\('),  # Left parenthesis.
+        ('RIGHT_PARENTHESI', r'\)'),  # Right parenthesis.
+        ('NEWLINE', r'\n'),  # Line ending.
+        ('SKIP', r'[ \t]+'),  # Skip over spaces and tabs.
+        ('MISMATCH', r'.'),  # Any other character.
+    ]
+    token_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_specification)
+    line = 1
+    line_start = 0
+    for match in re.finditer(token_regex, text):
+        kind = match.lastgroup
+        value = match.group()
+        column = match.start() - line_start + 1
+        if kind == 'NUMBER':
+            value = float(value) if '.' in value else int(value)
+        elif kind == 'NEWLINE':
+            line_start = match.end()
+            line += 1
+            continue
+        elif kind == 'SKIP':
+            continue
+        elif kind == 'MISMATCH':
+            raise err.SeaSubSyntaxError(f"Unexpected {value!r} on line {line}:{column}")
+        yield Token(kind, value, line, column)
+    yield Token('EOF', None, line, 0)
+
+
+class Token:
+    def __init__(self, token_type, value, line, column):
+        self.type = token_type
+        self.value = value
+        self.line = line
+        self.column = column
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value}, {self.line}, {self.column})"
+
+    def __str__(self):
+        return "<{}, {}>".format(self.type, self.value)

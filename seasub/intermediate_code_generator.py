539353053047cab80c40e205fb46212a060ad939
Fredrik Rose <fredrik.rose.developer@gmail.com>
2021-11-10 16:44:21 +0100

    Prepare the Quadruple class for target code generation
    The target code generator will need access to the symbol
    table and the members of the quadruple.

--- seasub/intermediate_code_generator.py
+++ seasub/intermediate_code_generator.py
@@ -18,17 +18,41 @@ def save_code(code, file_path):
 
 
 class Quadruple:
-    def __init__(self, operator, operand_1, operand_2, result):
+    def __init__(self, operator, operand_1, operand_2, result, symbol_table):
         self._operator = operator
         self._operand_1 = operand_1
         self._operand_2 = operand_2
         self._result = result
+        self._symbol_table = symbol_table
+
+    def __repr__(self):
+        return f"Quadruple({self._operator}, {self._operand_1}, {self._operand_2}, {self._result}, <SYMBOL TABLE>)"
 
     def __str__(self):
         def column(field):
             value = "-" if field is None else str(field)
             return f"{value}{' ' * (10 - len(value))}"
         return "".join(column(e) for e in (self._operator, self._operand_1, self._operand_2, self._result))
+
+    @property
+    def operator(self):
+        return self._operator
+
+    @property
+    def operand_1(self):
+        return self._operand_1
+
+    @property
+    def operand_2(self):
+        return self._operand_2
+
+    @property
+    def result(self):
+        return self._result
+
+    @property
+    def symbol_table(self):
+        return self._symbol_table
 
 
 class Generator(ast.NodeVisitor):
@@ -58,7 +82,7 @@ class Generator(ast.NodeVisitor):
         self._code = []
         self._functions[node.identifier] = self._code
         self._generic_visit(node)
-        self._code.append(Quadruple('q_label', self._current_label, None, None))
+        self._code.append(Quadruple('q_label', self._current_label, None, None, node.symbol_table))
         self._code = None
         self._current_label = None
 
@@ -69,14 +93,14 @@ class Generator(ast.NodeVisitor):
     def _visit_FunctionCall(self, node):
         arguments = [self.visit(arg) for arg in node.arguments]
         for arg in arguments:
-            self._code.append(Quadruple('q_param', arg, None, None))
+            self._code.append(Quadruple('q_param', arg, None, None, node.symbol_table))
         temp = self._generate_temp(node)
-        self._code.append(Quadruple('q_call', node.identifier.name, len(arguments), temp))
+        self._code.append(Quadruple('q_call', node.identifier.name, len(arguments), temp, node.symbol_table))
         return temp
 
     def _visit_ReturnStatement(self, node):
         value = self.visit(node.value)
-        self._code.append(Quadruple('q_return', self._current_label, value, None))
+        self._code.append(Quadruple('q_return', self._current_label, value, None, node.symbol_table))
 
     def _visit_Declaration(self, node):
         self._verify_type(node)
@@ -84,21 +108,21 @@ class Generator(ast.NodeVisitor):
 
     def _visit_Assignment(self, node):
         value = self.visit(node.value)
-        self._code.append(Quadruple('q_assign', value, None, node.identifier))
+        self._code.append(Quadruple('q_assign', value, None, node.identifier, node.symbol_table))
 
     def _visit_BinaryOperator(self, node):
         operators = {'+': 'q_plus', '-': 'q_minus', '*': 'q_mult', '/': 'q_div'}
         a = self.visit(node.a)
         b = self.visit(node.b)
         temp = self._generate_temp(node)
-        self._code.append(Quadruple(operators[node.operator], a, b, temp))
+        self._code.append(Quadruple(operators[node.operator], a, b, temp, node.symbol_table))
         return temp
 
     def _visit_UnaryOperator(self, node):
         operators = {'+': 'q_uplus', '-': 'q_uminus'}
         a = self.visit(node.a)
         temp = self._generate_temp(node)
-        self._code.append(Quadruple(operators[node.operator], a, None, temp))
+        self._code.append(Quadruple(operators[node.operator], a, None, temp, node.symbol_table))
         return temp
 
     def _visit_Identifier(self, node):
@@ -106,7 +130,7 @@ class Generator(ast.NodeVisitor):
 
     def _visit_IntegerConstant(self, node):
         temp = self._generate_temp(node)
-        self._code.append(Quadruple('q_load', node.value, None, temp))
+        self._code.append(Quadruple('q_load', node.value, None, temp, node.symbol_table))
         return temp
 
     def _visit_RealConstant(self, node):
